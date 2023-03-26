0fe78c2515c77f85703e91546197c3659efcdcf1
Dimchik <dimyrp@yandex.ru>
2023-03-26 13:44:40 +0300

    -settings py

--- back/data/settings.py
+++ back/data/settings.py
@@ -1,5 +1,5 @@
-import os
+# import os
 
-con_str = "mssql+pyodbc://:@DESKTOP-GL4ISM2/diplom1?driver=ODBC+Driver+17+for+SQL+Server" # LAPTOP-TK7JKUOV  DESKTOP-GL4ISM2
-directory = os.curdir
-time_offset = 1
+con_str = "mssql+pyodbc://:@LAPTOP-TK7JKUOV/diplom1?driver=ODBC+Driver+17+for+SQL+Server" # LAPTOP-TK7JKUOV  DESKTOP-GL4ISM2
+# directory = os.curdir
+# time_offset = 1
