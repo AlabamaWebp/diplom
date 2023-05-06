1c2b8cd4c58a62a62bc4c1275d31490eeb306eee
AlabamaWebp <dimyrp@yandex.ru>
2023-05-06 23:47:02 +0300

    1

--- back/main.py
+++ back/main.py
@@ -35,13 +35,17 @@ class User(BaseModel):
 
 # in production you can use Settings management
 # from pydantic to get secret key from .env
+
+
 class Settings(BaseModel):
     authjwt_secret_key: str = "secret"
+
 
 # callback to get your configuration
 @AuthJWT.load_config
 def get_config():
     return Settings()
+
 
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
@@ -53,6 +57,8 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
 # provide a method to create access tokens. The create_access_token()
 # function is used to actually generate the token to use authorization
 # later in endpoint protected
+
+
 @app.post('/login')
 def login(user: User, Authorize: AuthJWT = Depends()):
     if user.username != "test" or user.password != "test":
@@ -60,16 +66,34 @@ def login(user: User, Authorize: AuthJWT = Depends()):
 
     # subject identifier for who this token is for example id or username from database
     access_token = Authorize.create_access_token(subject=user.username)
-    return {"access_token": access_token}
+    refresh_token = Authorize.create_refresh_token(subject=user.username)
+    return {"access_token": access_token, "refresh_token": refresh_token}
+
 
 # protect endpoint with function jwt_required(), which requires
 # a valid access token in the request headers to access.
-@app.get('/user')
-def user(Authorize: AuthJWT = Depends()):
+@app.post('/refresh')
+def refresh(Authorize: AuthJWT = Depends()):
+    """
+    The jwt_refresh_token_required() function insures a valid refresh
+    token is present in the request before running any code below that function.
+    we can use the get_jwt_subject() function to get the subject of the refresh
+    token, and use the create_access_token() function again to make a new access token
+    """
+    Authorize.jwt_refresh_token_required()
+
+    current_user = Authorize.get_jwt_subject()
+    new_access_token = Authorize.create_access_token(subject=current_user)
+    return {"access_token": new_access_token}
+
+
+@app.get('/protected')
+def protected(Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
 
     current_user = Authorize.get_jwt_subject()
     return {"user": current_user}
+
 # jwt
 
 
