5c590551f8d66e1cca53ac8e4dcfae7a51faeff4
AlabamaWebp <dimyrp@yandex.ru>
2023-06-12 17:48:42 +0300

    123

--- back/data/API/a_users.py
+++ back/data/API/a_users.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi_jwt_auth import AuthJWT
+from sqlalchemy import select
 
 from data.BD.b_users import get_users, create_user, update_user, del_user, get_roles
-from data.BD.base import UniversalModel
+from data.BD.base import UniversalModel, User, engine
 from data.SCHEMAS.s_users import UserModel, UserCreateModel
 
 users = APIRouter()
@@ -12,18 +13,24 @@ users.prefix = "/users/"
 @users.get("")
 async def api_users(Authorize: AuthJWT = Depends()) -> list[UserModel]:
     Authorize.jwt_required()
+    if check_rights():
+        raise HTTPException(status_code=401, detail="Нет прав на выполнение операции")
     return get_users()
 
 
 @users.get("roles/")
 async def get_all_users(Authorize: AuthJWT = Depends()) -> list[UniversalModel]:
     Authorize.jwt_required()
+    if check_rights():
+        raise HTTPException(status_code=401, detail="Нет прав на выполнение операции")
     return get_roles()
 
 
 @users.post("create/")
 async def c_u(data: UserCreateModel, Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
+    if check_rights():
+        raise HTTPException(status_code=401, detail="Нет прав на выполнение операции")
     create_user(data)
     return "ok"
 
@@ -31,6 +38,8 @@ async def c_u(data: UserCreateModel, Authorize: AuthJWT = Depends()):
 @users.post("update/")
 async def u_u(id: int, data: UserCreateModel, Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
+    if check_rights():
+        raise HTTPException(status_code=401, detail="Нет прав на выполнение операции")
     update_user(id, data)
     return "ok"
 
@@ -38,5 +47,20 @@ async def u_u(id: int, data: UserCreateModel, Authorize: AuthJWT = Depends()):
 @users.post("delete/")
 async def d_u(id: int, Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
+    if check_rights():
+        raise HTTPException(status_code=401, detail="Нет прав на выполнение операции")
     del_user(id)
     return "ok"
+
+
+def check_rights(Authorize: AuthJWT = Depends()):
+    current_user = Authorize.get_jwt_subject()
+    query = select(User.c.RoleId).where(User.c.Login == current_user)
+    values = engine.execute(query).fetchone()
+    return_values = []
+    for i in values:
+        return_values.append(i)
+    if return_values[0] == 1:
+        return True
+    else:
+        return False
